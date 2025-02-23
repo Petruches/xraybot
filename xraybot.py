08e2fb068dd0c1e30d125ad9a85869df57e3d9d6
Petruches <petya.bykov.99@mail.ru>
2025-02-23 18:26:23 +0300

    add changes

--- xraybot.py
+++ xraybot.py
@@ -1,12 +1,35 @@
 import telebot
+import psutil
+
 
 #parsbot 
 bot = telebot.TeleBot('7939669010:AAEG8-KmNSVoisckbRFYM36Kq2-HkXJZWrU')
 
 
+@staticmethod
+def status() -> bool:
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+        if proc.info['name'] == "rsyslogd":
+            return proc.is_running()
+
+
+def info() -> str:
+    for proc in psutil.process_iter(['pid', 'name', 'username']):
+        if proc.info['name'] == "rsyslogd":
+            return str(proc)
+
+
 @bot.message_handler(commands=["status"])
-def start_message(message):
-    bot.send_message(message.chat.id, f"Привет чат\nВаше сообщение: {message.id}")
+def get_status(message):
+    if status() == True:
+        bot.send_message(message.chat.id, "Запущен")
+    else:
+        bot.send_message(message.chat.id, "Остановлен")
+
+
+@bot.message_handler(commands=["allstatus"])
+def all_status(message):
+    bot.send_message(message.chat.id, info())
 
 
 if __name__ == '__main__':
